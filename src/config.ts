d901837c4eea6c77d419dd77d1d4d94222fb3a01
brianberdoySSS <berdoyb13@gmail.com>
2026-02-10 11:45:58 -0300

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,59 @@
+export const siteConfig = {
+  name: "Brian Berdoy Schiavo",
+  title: "",
+  description: "Portfolio website of Brian Berdoy Schiavo",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "berdoyb13@gmail.com",
+    linkedin: "https://www.linkedin.com/in/brian-berdoy-schiavo-077440247",
+    cv: "/Brian_Berdoy_Schiavo_CV.pdf",
+    github: "https://github.com/brianberdoySSS",
+  },
+  aboutMe:
+    "Soy Analista de Seguridad Cloud con experiencia en entornos AWS, enfocado en el monitoreo,análisis y remediación de alertas de seguridad. Actualmente trabajo con servicios como IAM,WAF, VPC y S3, colaborando con equipos técnicos para fortalecer la postura de seguridad. Me encuentro cursando el Tercer año de la Licenciatura en Análisis de Sistemas, con interés en ciberseguridad, cloud computing y administración de sistemas.",
+  skills: ["Python", "AWS", "C++","Linux"],
+  projects: [
+    {
+      name: "Green Flag BS – Catálogo Web",
+      description:
+        "Sitio web de catálogo estático frontend creado para mostrar productos de audio con descripciones y precios, implementado en páginas de GitHub",
+      link: "https://greenman0001.github.io/green_flag_bs/",
+      skills: ["HTML", "CSS", "JavaScript"],
+    },
+    
+  ],
+  experience: [
+    {
+      company: "Tigo",
+      title: "Analista de Seguridad Cloud",
+      dateRange: "April 2025 - Present",
+      bullets: [
+        "Monitoreo y análisis de alertas de seguridad en entornos AWS, evaluando criticidad,riesgo e impacto organizacional.",
+        "Administración de accesos mediante IAM: gestión de usuarios, roles, grupos de seguridad y configuración de reglas WAF.",
+        "Colaboración con equipos DevOps en la gestión, depuración y control de cuentas y servicios activos en AWS.",
+      ],
+    },
+    
+  ],
+  education: [
+    {
+      school: "Universidad UTIC",
+      degree: "Licenciatura en Análisis de Sistemas",
+      dateRange: "2023 - 2027",
+      achievements: [
+        "Promedio actual: 4.0",
+        "Tercer año en curso",
+      
+      ],
+    },
+    {
+      school: "Centro Cultural Paraguayo Americano CCPA",
+      degree: "Formación en Inglés",
+      dateRange: "2023-2024",
+      achievements: [
+        "Niveles completados: P1, P2, PS1, PS4, I1",
+        "Enfoque en comprensión técnica y comunicación",
+      ],
+    },
+  ],
+};
